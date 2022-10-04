9066abee8e66be48eb9b671d6c42a066083807f1
Kevin Rodríguez <kevinrodriguezvalentin@gmail.com>
2022-10-04 20:36:13 +0100

    Main progressed

---
+++ frontend/src/components/log-in-comp/log-in-comp.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in-comp',
+  templateUrl: './log-in-comp.component.html',
+  styleUrls: ['./log-in-comp.component.scss']
+})
+export class LogInCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
