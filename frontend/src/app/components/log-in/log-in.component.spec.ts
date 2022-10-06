defa5cdcc60c5e550c326b9926ef4ced50c06997
Kevin Rodríguez <kevinrodriguezvalentin@gmail.com>
2022-10-06 20:44:04 +0100

    Component created

--- frontend/src/app/components/log-in/log-in.component.spec.ts
+++ frontend/src/app/components/log-in/log-in.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LogInCompComponent } from './log-in-comp.component';
+import { LogInComponent } from './log-in.component';
 
-describe('LogInCompComponent', () => {
-  let component: LogInCompComponent;
-  let fixture: ComponentFixture<LogInCompComponent>;
+describe('LogInComponent', () => {
+  let component: LogInComponent;
+  let fixture: ComponentFixture<LogInComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LogInCompComponent ]
+      declarations: [ LogInComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(LogInCompComponent);
+    fixture = TestBed.createComponent(LogInComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
